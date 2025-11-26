bb7b10d8fe3faa93d23bb069fc5ba83c692f33ec
Seif <britzgrank1@users.noreply.github.com>
2025-11-26 11:51:56 +0100

    data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+ System.out.println("Hello world");
 
+ DBconnector db = new DBconnector();
+ String url = "jdbc:sqlite:C:/Users/lullu/IntProjects/Databasetrening/identifier.sqlite";
+
+ db.connect(url);
+ db.selectAllPlayers();
+ db.addPlayer("Kjeld", 2000, 20, false);
     }
 }
